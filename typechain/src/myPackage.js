9808e3700a351c0bbb5e775e73edecaf7cac5dba
sener7106 <dskbbsm@gmail.com>
2022-07-26 16:22:50 +0900

    #5.5

---
+++ typechain/src/myPackage.js
@@ -0,0 +1,24 @@
+// @ts-check
+// JS-Doc 코멘트로 이루어진 문법
+
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+
+export function init(config) {
+  return true
+}
+
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns {number}
+ */
+
+export function exit(code) {
+  return code + 1
+}
