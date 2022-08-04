309916b626ae2f506e3880b987ee283f49657a2a
sener7106 <dskbbsm@gmail.com>
2022-08-04 15:24:32 +0900

    blockChain

--- typechain/build/index.js
+++ typechain/build/index.js
@@ -1,1 +1,13 @@
-const hello = () => "hi";
+class Block {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+    }
+}
+export {};
+// static method => 클래스에 없더라도 불러낼 수 있는 함수
--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,9 +1,47 @@
-import { init, exit } from 'myPackage'
+import crypto from 'crypto'
+interface BlockShape {
+  hash: string
+  prevHash: string
+  height: number
+  data: string
+}
+class Block implements BlockShape {
+  public hash: string
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string,
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data)
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`
+    return crypto.createHash('sha256').update(toHash).digest('hex')
+  }
+}
 
-init({
-  url: 'true',
-})
+// static method => 클래스에 없더라도 불러낼 수 있는 함수
+class BlockChain {
+  private blocks: Block[]
+  constructor() {
+    this.blocks = []
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return ''
+    return this.blocks[this.blocks.length - 1].hash
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+    this.blocks.push(newBlock)
+  }
+  public getBlocks() {
+    return [...this.blocks]
+  }
+}
 
-exit(1)
+const blockChain = new BlockChain()
+blockChain.addBlock('First One')
+blockChain.addBlock('Second One')
+blockChain.addBlock('Third One')
 
-localStorage.clear()
+console.log(blockChain.getBlocks())
